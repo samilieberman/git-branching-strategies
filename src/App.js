3aa40773ab4eec9ba24f73fac2e764e8cf166b2a
Thomas Olinger <olingerthomas95@gmail.com>
2024-09-07 12:59:48 -0400

    1.0.0
    Added name

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Thomas Olinger and this is my project </p>
       </header>
     </div>
   );
